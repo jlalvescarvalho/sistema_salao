2f934ada3e3735ac4882adce63b0afd1be30e806
FSOFTSistemas <fsoftsistemas@gmail.com>
2023-11-02 23:44:29 -0300

    criado service de produto e rotas, inicio de crud de produto AJUSTAR LOGIN

--- database/migrations/2014_10_12_000003_create_users_table.php
+++ database/migrations/2014_10_12_000003_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger("id_empresa")->nullable();
+            $table->foreign("id_empresa")->references('id')->on('empresas');
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/views/produto/index.blade.php
+++ resources/views/produto/index.blade.php
@@ -7,7 +7,26 @@
 @stop
 
 @section('content')
-    
+    <table id="produtos" class="display">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>CODBARRAS</th>
+                <th>DESCRIÇÃO</th>
+                <th>UN</th>
+                <th>PREÇO CUSTO</th>
+                <th>PREÇO VENDA</th>
+                <th>ESTOQUE</th>
+                <th>AÇÕES</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Row 1 Data 1</td>
+                <td>Row 1 Data 2</td>
+            </tr>
+        </tbody>
+    </table>
 @stop
 
 @section('css')
