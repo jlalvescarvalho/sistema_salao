4553bc465a1c6e2426c5920dfeab06f15d85289b
FSOFTSistemas <fsoftsistemas@gmail.com>
2024-01-27 21:20:03 -0300

    ajustes de segunrança nas rotas

--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
 
-//Produto
-Route::resource('produtos', App\Http\Controllers\ProdutoController::class)->except(['show']);
-Route::resource('clientes', App\Http\Controllers\ClienteController::class)->except(['show']);
-Route::resource('servicos', App\Http\Controllers\ServicoController::class)->except(['show']);
-Route::resource('pacotes', App\Http\Controllers\PacoteController::class)->except(['show']);
-Route::resource('agendamentos', App\Http\Controllers\AgendamentoController::class)->except(['destroy', 'show']);
-Route::resource('agendamentos-de-pacotes', App\Http\Controllers\AgendamentoPacoteController::class)->names('pacotes.agendamentos')->except(['destroy', 'show']);
-Route::resource('contratos', App\Http\Controllers\ContratoPacoteController::class)->except(['destroy', 'show']);
-Route::post('contratos/{id}/cancelar', [App\Http\Controllers\ContratoPacoteController::class, 'cancelar'])->name('contratos.cancelar');
+Route::resource('produtos', App\Http\Controllers\ProdutoController::class)->except(['show'])->middleware('auth');
+Route::resource('clientes', App\Http\Controllers\ClienteController::class)->except(['show'])->middleware('auth');
+Route::resource('servicos', App\Http\Controllers\ServicoController::class)->except(['show'])->middleware('auth');
+Route::resource('pacotes', App\Http\Controllers\PacoteController::class)->except(['show'])->middleware('auth');
+Route::resource('agendamentos', App\Http\Controllers\AgendamentoController::class)->except(['destroy', 'show'])->middleware('auth');
+Route::resource('agendamentos-de-pacotes', App\Http\Controllers\AgendamentoPacoteController::class)->names('pacotes.agendamentos')->except(['destroy', 'show'])->middleware('auth');
+Route::resource('contratos', App\Http\Controllers\ContratoPacoteController::class)->except(['destroy', 'show'])->middleware('auth');
+Route::post('contratos/{id}/cancelar', [App\Http\Controllers\ContratoPacoteController::class, 'cancelar'])->name('contratos.cancelar')->middleware('auth');
